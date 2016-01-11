86dbfe457a8393ad99fb6e1e8c5376293272a2d4
Jefferson Choi <jeffersonchoi@gmail.com>
2016-01-11 14:08:58 -0800

    merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-	enum role: [:registered_user, :staff_user, :admin, :super_admin]
 
-=======
 	enum role: [:register_user, :staff_user, :admin, :super_admin]
->>>>>>> temp
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
