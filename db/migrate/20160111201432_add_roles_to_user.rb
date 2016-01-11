664d30ad55ad736445ccab99e8aea395047b3be0
Jefferson Choi <jeffersonchoi@gmail.com>
2016-01-11 14:09:33 -0800

    merge conflicts

--- db/migrate/20160111201432_add_roles_to_user.rb
+++ db/migrate/20160111201432_add_roles_to_user.rb
@@ -1,9 +1,5 @@
 class AddRolesToUser < ActiveRecord::Migration
   def change
-<<<<<<< HEAD
-    add_column :users, :role, :integer, default: 0
-=======
   	add_column :users, :role, :integer, default: 0
->>>>>>> temp
   end
 end
