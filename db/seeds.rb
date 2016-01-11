144717808e561f7e2cb8b23e62898406399d0a0f
Jefferson Choi <jeffersonchoi@gmail.com>
2016-01-11 12:19:08 -0800

    edit seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-u1 = User.create(email: 'jefferson@test.com', password: 'Password1!', first_name: 'Jefferson', last_name: 'Choi', gender: 'male', dob: Date.new(1991,9,12))
-u2 = User.create(email: 'shadow@test.com', password: 'Password1!', first_name: 'Shadow', last_name: 'Wong', gender: 'female', dob: Date.new(1993,7,19)) 
+u1 = User.create(email: 'jefferson@test.com', password: 'Password1!', first_name: 'Jefferson', last_name: 'Choi', gender: 'male', dob: Date.new(1991,9,12), role: 3)
+u2 = User.create(email: 'shadow@test.com', password: 'Password1!', first_name: 'Shadow', last_name: 'Wong', gender: 'female', dob: Date.new(1993,7,19), role: 3) 
