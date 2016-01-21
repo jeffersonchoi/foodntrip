503098e59f97f0e6fc69d8d50b75e816f65dcad9
Jefferson Choi <jeffersonchoi@gmail.com>
2016-01-21 13:07:24 -0800

    set up model relationship

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ActiveRecord::Base
+  has_many :userplaces
+  has_many :users, through: :userplaces
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+	has_many :userplaces
+	has_many :places, through: :userplaces
 end
