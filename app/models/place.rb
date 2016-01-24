8ac60613cb384bf22d6cd8b260b7b41c56e9c8fb
Jefferson Choi <jeffersonchoi@gmail.com>
2016-01-23 22:02:29 -0800

    wordy if statements

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,14 @@
 class Place < ActiveRecord::Base
   has_many :userplaces
   has_many :users, through: :userplaces
+
+  # def myhelper(*args)
+  #   obj = self
+  #   Array(args).each do |arg|
+  #     return nil unless self.respond_to? arg
+  #     obj = obj.send(arg)
+  #   end
+  #   return obj
+  # end
+
 end
